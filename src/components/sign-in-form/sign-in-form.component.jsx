bb2bf6870b9af22fc6145d37606788ca401dffe5
JPachecoZ <jepach@gmail.com>
2023-09-14 22:43:33 -0500

    feat: add styled components

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 
 import FormInput from '../form-input/form-input.component'
-import Button from '../button/button.component'
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component'
 
-import { signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils'
+import { signInWithGooglePopup, signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils'
 
 import './sign-in-form.styles.scss'
 
@@ -66,9 +66,8 @@ const SignInForm = () => {
 
                 <div className='buttons-container'>
                     <Button type="submit">Sign In</Button>
-                    <Button type="button" buttonType="google" onClick={signInWithGoogle}>Google sing in</Button>
+                    <Button type="button" buttonType={BUTTON_TYPE_CLASSES.google} onClick={signInWithGoogle}>Google sing in</Button>
                 </div>
-                
             </form>
         </div>
     )
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,5 @@
 import { Fragment, useContext } from "react"
-import { Outlet, Link } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 
 import CartIcon from '../../components/cart-icon/cart-icon.component'
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component"
@@ -10,7 +10,7 @@ import { CartContext } from "../../contexts/cart.context"
 
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 
-import './navigation.styles.scss'
+import { NavigationContainer, NavLinks, NavLink, LogoContainer } from './navigation.styles.jsx'
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext)
@@ -22,30 +22,30 @@ const Navigation = () => {
 
     return (
       <Fragment>
-        <div className="navigation">
-            <Link className="logo-container" to='/'>
+        <NavigationContainer>
+            <LogoContainer to='/'>
                 <CrwnLogo className="logo"/>
-            </Link>
+            </LogoContainer>
             
-            <div className="nav-links-container">
-                <Link className="nav-link" to='/shop'>
+            <NavLinks>
+                <NavLink to='/shop'>
                     SHOP
-                </Link>
+                </NavLink>
                 { 
                     currentUser ? (
-                    <span className="nav-link" onClick={signOutHandler}>
+                    <NavLink as='span' onClick={signOutHandler}>
                         SIGN OUT
-                    </span>
+                    </NavLink>
                     ) : (
-                    <Link className="nav-link" to='/auth'>
+                    <NavLink to='/auth'>
                         SIGN IN
-                    </Link>
+                    </NavLink>
                     )
                 }
                 <CartIcon/>
-            </div>
+            </NavLinks>
             {isCartOpen && <CartDropdown/>}
-        </div>
+        </NavigationContainer>
         <Outlet/>
       </Fragment>
     )
